d88f2913c84af89619e7b6a6a076d1f0de453e26
Telicia Watson <teli203@aol.com>
2020-12-29 20:06:38 -0600

    add image to read me

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,23 +1,24 @@
-const router = require("express").Router();
-const store = require("../db/store");
+//dependencies//
+const noteData = require("../db/db.json");
+
+//router with api GET request//
+module.exports = function (app) {
+
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
+}
 
 // GET "/api/notes" responds with all notes from the database
-router.get("/notes", (req, res) => {
-  store
-    .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
-});
-
-router.post("/notes", (req, res) => {
-  store
+app.post("/api/notes", (req, res) => {
+  store, noteData.push(req.body)
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", (req, res) => {
+app.delete("/app/notes/:id", (req, res) => {
   store
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
